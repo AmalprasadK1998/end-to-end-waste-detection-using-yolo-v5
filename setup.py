5e946219b372e6ecb1b8d4fa413b362ecb70c123
AmalprasadK1998 <amalprasadk1998@gmail.com>
2024-03-06 12:19:40 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'WasteDetection',
+    version= '0.0.0',
+    author= 'Amal Prasad',
+    author_email= 'amalprasadk1998@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
